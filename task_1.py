e7cb4c3b91c7c8ce58b8b20278d462332d33ccf3
Erkinov Sardor <you@example.com>
2025-07-09 11:20:13 +0500

    uyga vazifa qilindi

--- task_1.py
+++ task_1.py
@@ -12,7 +12,7 @@ def task_1(x):
         return x+1
     else:
         return x-2
-    print(task_1(2))
+print(task_1(2))
 # Example:
 # task_1(1) -> 1
 # task_1(-4) -> -2  
@@ -28,8 +28,11 @@ def task_2(x):
     Returns:
         int
     """
-    return x
-
+    if x>0:
+        return x+1
+    else:
+        return x+2
+print(task_2(0)) 
 # Example:
 # task_2(1) -> 2
 # task_2(-4) -> -6
@@ -45,8 +48,11 @@ def task_3(x):
     return:
         int
     """
-    return x
-
+    if x>0:
+        return x+1
+    else:
+        return x-2
+print(task_3(0))
 
 # Example:
 # task_3(1) -> 1
@@ -63,7 +69,19 @@ def task_4(a,b,c):
     return:
         int
     """
-    return 0
+    if a>0:
+        return a
+    else:
+        return 0
+    if b>0:
+        return b
+    else:
+        return 0
+    if c>0:
+        return c
+    else :
+        return 0
+print(task_4(1,-3,4))    
 
 # Example:
 # task_4(1,2,3) -> 6
@@ -80,10 +98,21 @@ def task_5(a,b,c):
     return:
         int
 
-
     """
-    return 0
-
+    xotira = 0
+    if a>0:
+        return  xotira + 1
+    else:
+        return  xotira
+    if b>0:
+        return  xotira + 1
+    else:
+        return  xotira
+    if c>0:
+        return  xotira + 1
+    else:
+        return  xotira
+print(task_5(1,2,-3))    
 # Example:
 # task_5(1,2,3) -> 3
 # task_5(-1,2,3) -> 2
